632b414f8789c486dd4b6edc9aa8675381983607
skooger <adamjskooglambda@gmail.com>
2019-11-11 12:01:54 -0800

    completed get older cars, german cars and arrow functions

--- index.js
+++ index.js
@@ -240,8 +240,17 @@ function getModelYears(inventory) {
  * with a `car_year` which is at most the given desired max year,
  * in the same order as they appear in the original inventory.
 */
-function getOlderCars(/* code here */) {
-  /* code here */
+function getOlderCars(inventory,maxYr) {
+  let olderCars = []
+  for(var i=0;i<inventory.length;i++)
+  {
+    if(inventory[i].car_year <= maxYr)
+    {
+      olderCars.push(inventory[i])
+    }
+  }
+  return olderCars
+  
 }
 
 /**
@@ -255,8 +264,17 @@ function getOlderCars(/* code here */) {
  * made by either `Audi` or `Mercedes-Benz` or `Volkswagen` or `BMW`,
  * in the same order as they appear in the original inventory.
 */
-function getGermanCars(/* code here */) {
-  /* code here */
+function getGermanCars(inventory) {
+  let germanCars = []
+  for(var i=0;i<inventory.length;i++)
+  {
+    if(inventory[i].car_make == "Audi" || inventory[i].car_make == "Mercedes-Benz" || 
+        inventory[i].car_make == "Volkswagen" || inventory[i].car_make == "BMW")
+    {
+      germanCars.push(inventory[i])
+    }
+  }
+  return germanCars
 }
 
 /**
@@ -265,9 +283,7 @@ function getGermanCars(/* code here */) {
  * @instructions
  * Create arrow function versions of the following commented-out functions:
  * 
- * const sum = function (a, b) {
- *   return a + b
- * }
+ * const sum = 
  * 
  * const addFive = function(num) {
 *    return num + 5
@@ -277,9 +293,13 @@ function getGermanCars(/* code here */) {
  *   return num * 2
  * }
 */
-const sum = null; // code here!
-const addFive = null; // code here!
-const argTimesTwo = null; // code here!
+const sum =  (a, b) => {
+  return a + b }
+
+const addFive = (num) => {
+  return num + 5 }
+const argTimesTwo =  (num) => {
+  return num * 2 }
 
 /**
  * ### Challenge `carMaker`
